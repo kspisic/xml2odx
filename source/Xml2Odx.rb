d95acf2b133a53fe626594af64bfc972c1aa1278
kspisic <keko@spisic.de>
2014-10-15 00:57:46 +0200

    Nice and fency UI with Methadone lib. Parsing multiple data files

--- source/Xml2Odx.rb
+++ source/Xml2Odx.rb
@@ -1,28 +1,19 @@
 require 'nokogiri'
 require 'fileutils'
+require 'methadone'
 require_relative 'OdxTemplates'
 
-CvdtInputFile = ARGV[0]
-OdxTemplateInputFile = ARGV[1]
-OdxOutputFile = ARGV[2]
+include Methadone::Main
+include Methadone::CLILogging
+
+
+#CvdtInputFile = ARGV[0]
+#OdxTemplateInputFile = ARGV[1]
+#OdxOutputFile = ARGV[2]
 
 $id = 10000
 ID_REF_FunctionalClass = '_161' # 161 --> Stored data
 ID_REF_Nrc			   = '_106'
-
-begin
-	f1 = File.open(CvdtInputFile, 'rb')
-	f2 = File.open(OdxTemplateInputFile, 'rb')
-	f3 = File.open(OdxOutputFile, 'w')
-rescue 
-	puts "File not found"
-	exit 1
-end
-	
-xml_in1 = Nokogiri::XML(f1)
-xml_in2 = Nokogiri::XML(f2)
-f1.close
-f2.close
 
 
 def getDidParams(subdata_nodes)
@@ -54,83 +45,155 @@ def getDidParam(subdata)
 end
 
 
-dataArray = []
+main do |cvdt_xml|
+	
+	begin
+	f1 = File.open(cvdt_xml, 'rb')
+	f2 = File.open(options['template'], 'rb')
+	f3 = File.open(options['output'], 'w')
+	rescue => e
+		puts e
+		f1.close unless f1.nil?
+		f2.close unless f2.nil?
+		f3.close unless f3.nil?
+		exit 1
+	end
+		
+	xml_in1 = Nokogiri::XML(f1)
+	xml_in2 = Nokogiri::XML(f2)
+	f1.close
+	f2.close
 
-xml_in1.xpath("//Data").map do |node|
+	xml_file_array = []
+	xml_in1.xpath("//PartialSet").map do |xml_file|
+		f_hash = { :file_name			=> xml_file.xpath('Path').text.gsub('..\\', ''),
+				   :file_type		 	=> xml_file.xpath('Type').text   }
+		xml_file_array.push(f_hash)
+		
+		puts "Will use '#{f_hash[:file_name]}' as <<#{f_hash[:file_type]}>>"
+	end
 	
-	did_st = node.xpath('ID').text
-	did = if did_st.include? "0x" then did_st.gsub('0x','').to_i(16).to_s else did_st end
+	if xml_file_array.empty? then 
+		puts "Problem with #{cvdt_xml}: Could not detect <PartialSetList> Node in XML file"
+		exit 1
+	end
 	
-	hash = {	:DID_name 			=> node.xpath('Name').text.gsub(/[^0-9A-Za-z]/, '_'),
-				:DID_desc 			=> "Supported Variants: #{node.xpath('SupportedVariants').text}",
-				:DID_id 			=> did,
-				:DID_rw				=> node.xpath('ReadWriteMode').text,
-				:DID_byte_size		=> node.xpath('Length').text,
-				:DID_struct_ref_id 	=> 0, 
-				:RQ_id				=> 0,
-				:POSRESP_id			=> 0,
-				:NEGRESP_id			=> 0,
-				:DID_params			=> getDidParams(node.xpath('SubData'))
-			}
+	dataArray = []
+	
+	xml_file_array.map do |xml_file|
+
+		if xml_file[:file_type] == "Data" then
+	
+			begin
+				fh = File.open(xml_file[:file_name], "rb")
+				puts "Parsing #{xml_file[:file_name]}..."
+			rescue => e
+				puts e.to_s + '---> Will ignore this file'
+				next
+			end
 			
-	dataArray.push(hash)
-		  
+			xml_in = Nokogiri::XML(fh)
+			fh.close
+	
+			xml_in.xpath("//Data").map do |node|
+				
+				did_st = node.xpath('ID').text
+				did = if did_st.include? "0x" then did_st.gsub('0x','').to_i(16).to_s else did_st end
+				
+				hash = {	:DID_name 			=> node.xpath('Name').text.gsub(/[^0-9A-Za-z]/, '_'),
+							:DID_desc 			=> "Supported Variants: #{node.xpath('SupportedVariants').text}",
+							:DID_id 			=> did,
+							:DID_rw				=> node.xpath('ReadWriteMode').text,
+							:DID_byte_size		=> node.xpath('Length').text,
+							:DID_struct_ref_id 	=> 0, 
+							:RQ_id				=> 0,
+							:POSRESP_id			=> 0,
+							:NEGRESP_id			=> 0,
+							:DID_params			=> getDidParams(node.xpath('SubData'))
+						}
+						
+				dataArray.push(hash)
+					  
+			end # xml_in1
+		
+		elsif xml_file[:file_type] == "DTC" then
+			puts "Warning: DTCs not yet handled. " + xml_file[:file_name] + " ---> Will ignore this file"
+		else
+			puts "Warning: Type '#{xml_file[:file_type]}' not supported yet." + xml_file[:file_name] + " ---> Will ignore this file"
+		end # if Data
+	end # xml_file_array
+
+	#puts dataArray[0][:DID_params]
+
+	puts "Warning: Could not find any useful data in parsed files" if dataArray.empty?
+	puts dataArray if options[:verbose]
+	
+	# Find nodes in xml template file
+	request_node = xml_in2 %('//REQUESTS')
+	posresp_node = xml_in2 %('//POS-RESPONSES')
+	negresp_node = xml_in2 %('//NEG-RESPONSES')
+	diagcomms_node = xml_in2 %('//DIAG-COMMS')
+	structures_node = xml_in2 %('//STRUCTURES')
+	dop_node = xml_in2 %('//DATA-OBJECT-PROPS')
+
+	dataArray.each{ |did|
+		
+		dop_node.last_element_child.after(getTemplate_Dops(did))
+		
+		structures_node.last_element_child.after(getTemplate_Structure(did))
+		did[:DID_struct_ref_id] = $id
+		$id = $id + 1;
+		
+		if did[:DID_rw].include? "Read"	
+			request_node.last_element_child.after(getTemplate_Read_Request("BA", "00", did))
+			did[:RQ_id] = $id;
+			$id = $id + 1;
+			
+			posresp_node.last_element_child.after(getTemplate_Read_PosResp("BA", "00", did))
+			did[:POSRESP_id] = $id;
+			$id = $id + 1;
+
+			negresp_node.last_element_child.after(getTemplate_Read_NegResp("BA", "00", did))
+			did[:NEGRESP_id] = $id;
+			$id = $id + 1;	
+			
+			diagcomms_node.last_element_child.after(getTemplate_DiagComms(did, "Read"))
+			$id = $id + 3;
+		end
+		
+		if did[:DID_rw].include? "Write"
+			request_node.last_element_child.after(getTemplate_Write_Request("BA", "01", did))
+			did[:RQ_id] = $id;
+			$id = $id + 1;
+			
+			posresp_node.last_element_child.after(getTemplate_Write_PosResp("BA", "01", did))
+			did[:POSRESP_id] = $id;
+			$id = $id + 1;
+
+			negresp_node.last_element_child.after(getTemplate_Write_NegResp("BA", "01", did))
+			did[:NEGRESP_id] = $id;
+			$id = $id + 1;	
+			
+			diagcomms_node.last_element_child.after(getTemplate_DiagComms(did, "Write"))
+			$id = $id + 3;
+		end
+	}
+
+	f3.write(xml_in2.to_xml)
+	f3.close
+	puts "Finished!"
 end
 
-puts dataArray[0][:DID_params]
+version     '0.0.1'
+description "Converts VDTXML files into ODX 2.2.0 by KS \nNo warranty. Use at own risk."
+arg         :cvdt_xml, :required
 
-# Find nodes in xml template file
-request_node = xml_in2 %('//REQUESTS')
-posresp_node = xml_in2 %('//POS-RESPONSES')
-negresp_node = xml_in2 %('//NEG-RESPONSES')
-diagcomms_node = xml_in2 %('//DIAG-COMMS')
-structures_node = xml_in2 %('//STRUCTURES')
-dop_node = xml_in2 %('//DATA-OBJECT-PROPS')
+on("--verbose","Be verbose")
 
-dataArray.each{ |did|
-	#puts getTemplate_Dops(did)
-	
-	dop_node.last_element_child.after(getTemplate_Dops(did))
-	
-	structures_node.last_element_child.after(getTemplate_Structure(did))
-	did[:DID_struct_ref_id] = $id
-	$id = $id + 1;
-	
-	if did[:DID_rw].include? "Read"	
-		request_node.last_element_child.after(getTemplate_Read_Request("BA", "00", did))
-		did[:RQ_id] = $id;
-		$id = $id + 1;
-		
-		posresp_node.last_element_child.after(getTemplate_Read_PosResp("BA", "00", did))
-		did[:POSRESP_id] = $id;
-		$id = $id + 1;
+options['template'] = "Template.odx-d"
+on("-t ODX_TEMPLATE", "--template", "File will be used as ODX template for output")
 
-		negresp_node.last_element_child.after(getTemplate_Read_NegResp("BA", "00", did))
-		did[:NEGRESP_id] = $id;
-		$id = $id + 1;	
-		
-		diagcomms_node.last_element_child.after(getTemplate_DiagComms(did, "Read"))
-		$id = $id + 3;
-	end
-	
-	if did[:DID_rw].include? "Write"
-		request_node.last_element_child.after(getTemplate_Write_Request("BA", "01", did))
-		did[:RQ_id] = $id;
-		$id = $id + 1;
-		
-		posresp_node.last_element_child.after(getTemplate_Write_PosResp("BA", "01", did))
-		did[:POSRESP_id] = $id;
-		$id = $id + 1;
+options['output'] = "output.odx-d"
+on("-o ODX_OUTPUT", "--output", "ODX_OUTPUT = ODX_TEMPLATE + cvdt_xml")
 
-		negresp_node.last_element_child.after(getTemplate_Write_NegResp("BA", "01", did))
-		did[:NEGRESP_id] = $id;
-		$id = $id + 1;	
-		
-		diagcomms_node.last_element_child.after(getTemplate_DiagComms(did, "Write"))
-		$id = $id + 3;
-	end
-}
-
-
-f3.write(xml_in2.to_xml)
-f3.close
+go!
